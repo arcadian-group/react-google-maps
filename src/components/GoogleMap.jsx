ad7ee34ce2225104c278aad5686992c7fe865bde
Dylan Pinn <dylan@arcadiandigital.com.au>
2019-03-12 18:00:43 +1100

    chore(src/components): compile from src/macros with `babel`

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -195,11 +195,6 @@ export class Map extends React.PureComponent {
     /**
      * function
      */
-    onResize: PropTypes.func,
-
-    /**
-     * function
-     */
     onTiltChanged: PropTypes.func,
 
     /**
@@ -244,6 +239,14 @@ export class Map extends React.PureComponent {
     return this.context[MAP].panToBounds(...args)
   }
 
+  /**
+   * @see https://developers.google.com/maps/documentation/javascript/3.exp/reference#Map
+   * @public
+   */
+  setZoom(...args) {
+    return this.context[MAP].setZoom(...args)
+  }
+
   /*
    * @see https://developers.google.com/maps/documentation/javascript/3.exp/reference#Map
    */
@@ -275,7 +278,7 @@ export class Map extends React.PureComponent {
 
   /**
    * Returns the lat/lng bounds of the current viewport. If more than one copy of the world is visible, the bounds range in longitude from -180 to 180 degrees inclusive. If the map is not yet initialized (i.e. the mapType is still null), or center and zoom have not been set then the result is `null` or `undefined`.
-   * @type LatLngBoundsnullundefined
+   * @type LatLngBounds
    * @public
    */
   getBounds() {
@@ -284,7 +287,7 @@ export class Map extends React.PureComponent {
 
   /**
    * Returns the position displayed at the center of the map. Note that this `LatLng` object is _not_ wrapped. See `[LatLng](#LatLng)` for more information.
-   * @type LatLngLatLngLatLng
+   * @type LatLng
    * @public
    */
   getCenter() {
@@ -329,7 +332,7 @@ export class Map extends React.PureComponent {
 
   /**
    * Returns the current `Projection`. If the map is not yet initialized (i.e. the mapType is still null) then the result is null. Listen to `projection_changed` and check its value to ensure it is not null.
-   * @type ProjectionProjectionprojection_changed
+   * @type Projection
    * @public
    */
   getProjection() {
@@ -338,7 +341,7 @@ export class Map extends React.PureComponent {
 
   /**
    * Returns the default `StreetViewPanorama` bound to the map, which may be a default panorama embedded within the map, or the panorama set using `setStreetView()`. Changes to the map's `streetViewControl` will be reflected in the display of such a bound panorama.
-   * @type StreetViewPanoramaStreetViewPanoramasetStreetView()streetViewControl
+   * @type StreetViewPanorama
    * @public
    */
   getStreetView() {
@@ -347,7 +350,7 @@ export class Map extends React.PureComponent {
 
   /**
    * Returns the current angle of incidence of the map, in degrees from the viewport plane to the map plane. The result will be `0` for imagery taken directly overhead or `45` for 45° imagery. 45° imagery is only available for `satellite` and `hybrid` map types, within some locations, and at some zoom levels. **Note:** This method does not return the value set by `setTilt`. See `setTilt` for details.
-   * @type number045satellitehybridsetTiltsetTilt
+   * @type number
    * @public
    */
   getTilt() {
@@ -385,7 +388,6 @@ const eventMap = {
   onHeadingChanged: "heading_changed",
   onIdle: "idle",
   onProjectionChanged: "projection_changed",
-  onResize: "resize",
   onTiltChanged: "tilt_changed",
   onZoomChanged: "zoom_changed",
 }
